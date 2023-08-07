22c1761410a32d302e6495f18e43dce21e35bc4c
Maulik Savaliya <maulik@7span.com>
2023-08-07 14:54:15 +0530

    Create .env files for all environments || Restructure of Home page component.

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
-import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 import "./globals.css";
 
 export default function Home() {
   return (
     <main>
-      <Banner />
-      <Footer/>
+      <div className="relative">
+        <Header className="absolute inset-x-0" />
+        <Hero />
+      </div>
+      <Footer />
     </main>
   );
 }
