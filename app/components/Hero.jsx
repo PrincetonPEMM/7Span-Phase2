952316e78cd470a6e564d4e03ab1ff121cb12190
keya-7span <keya@7span.com>
2023-08-07 14:39:47 +0530

    home page design

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -46,6 +46,7 @@ const Hero = () => {
       [index]: !prevState[index],
     }));
   };
+
   const stories = [
     {
       img: img1,
@@ -73,56 +74,127 @@ const Hero = () => {
     <>
       <div className="grid md:grid-cols-3">
         {bannerItems.map((item, index) => (
-          <div key={index} className="relative">
+          <div key={index} className="relative ">
             <button
-              className="relative flex h-full text-left z-30"
+              className="w-full aspect-auto md:h-full"
               onClick={() => {
                 bannerIndex || bannerIndex === 0
                   ? setBannerIndex(null)
                   : setBannerIndex(index);
                 toggleseDesc(index); // Toggle the detailed view
-                toggleContent(index); // Also toggle the main content if needed
+                // Also toggle the main content if needed
+                // setBannerIndex(index);
               }}
             >
-              <Image
-                src={item.img}
-                alt="Picture of the author"
-                style={{
-                  objectFit: "cover",
-                  objectPosition: "center",
-                  width: "100%",
-                  height: "100%",
-                }}
-              />
-              <div className="absolute m-auto top-1/3 z-10 text-white px-10">
-                <span className="text-sm lg:text-xl font-bold uppercase mr-1">
-                  featured
-                </span>
-                <h3
-                  className="text-2xl lg:text-5xl leading-tight font-header uppercase"
-                  onClick={() => toggleContent(index)}
-                >
-                  {item.title}
-                </h3>
-                {isOpen[index] && (
-                  <p
-                    className={`text-xs lg:text-base mt-1 ${
-                      isOpen ? "slide-in" : "slide-out"
-                    }`}
+              <div className="relative flex h-full text-left z-30">
+                <Image
+                  src={item.img}
+                  alt="Picture of the author"
+                  style={{
+                    objectFit: "cover",
+                    objectPosition: "center",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+                <div className="absolute m-auto top-1/3 z-10 text-white px-10">
+                  <span className="text-sm lg:text-xl font-bold uppercase mr-1">
+                    featured
+                  </span>
+                  <h3
+                    className="text-2xl lg:text-5xl leading-tight font-header uppercase"
+                    onClick={() => toggleContent(index)}
                   >
-                    {item.text}
-                  </p>
-                )}
+                    {item.title}
+                  </h3>
+
+                  <p className="text-xs lg:text-base mt-1">{item.text}</p>
+                </div>
               </div>
             </button>
             {/* Show the detailed view for the clicked item */}
+
+            <div className="md:hidden block">
+              {isOpen[index] ||
+                (bannerIndex != null && stories[bannerIndex] ? (
+                  stories.length &&
+                  stories.map((item, storyIndex) => {
+                    return bannerIndex === index ? (
+                      <React.Fragment key={storyIndex}>
+                        <div className="md:flex relative bg-secondary-500 text-center md:text-left">
+                          <div className="relative aspect-square lg:aspect-auto max-w-xs lg:max-w-none mx-auto md:mr-0 md:w-3/6 lg:max-h-[600px]">
+                            <Image
+                              src={item.img}
+                              alt="Picture of the author"
+                              style={{
+                                objectFit: "cover",
+                                objectPosition: "center top",
+                                width: "100%",
+                                height: "100%",
+                                "@media (max-width: 1024px)": {
+                                  height: "auto",
+                                  aspectRaio: "1/1",
+                                  maxWidth: "100%",
+                                },
+                              }}
+                            />
+                          </div>
+                          <div className="w-full col-span-2 flex text-white bg-secondary-500">
+                            <div className=" z-10 space-y-4 p-10 max-w-3xl">
+                              <h3 className="text-2xl lg:text-5xl font-bold font-body">
+                                Featured{item.title}
+                              </h3>
+                              <p className="text-sm md:text-base lg:text-xl">
+                                {item.text}
+                              </p>
+                            </div>
+                            <button
+                              className="absolute top-5 right-5 left-auto bottom-auto text-black"
+                              onClick={() => {
+                                setBannerIndex(null);
+                              }}
+                            >
+                              <MdiClose />
+                            </button>
+                          </div>
+                        </div>
+                        <div className="grid md:grid-cols-3">
+                          {item.storyPart &&
+                            item.storyPart.map((data, subIndex) => (
+                              <div
+                                className="relative items-center justify-center uppercase text-center"
+                                key={subIndex}
+                              >
+                                <Image
+                                  src={data.img}
+                                  alt="Picture of the author"
+                                  sizes="(max-width: 768px) 30vw, (max-width: 1200px) 30vw"
+                                />
+                                <div className="absolute flex items-center justify-center z-10 text-white space-y-4 px-10 w-full inset-0 bg-black bg-opacity-50 ">
+                                  <span className="text-lg lg:text-2xl font-bold font-body">
+                                    Story 1{data.title}
+                                  </span>
+                                </div>
+                              </div>
+                            ))}
+                        </div>
+                      </React.Fragment>
+                    ) : null;
+                  })
+                ) : (
+                  <div className="flex items-center justify-normal text-2xl">
+                    {/* <p>No data found</p> */}
+                  </div>
+                ))}
+            </div>
           </div>
         ))}
       </div>
 
       {/* The below section will appear when each story event triggers */}
-      <div>
-        {stories &&
+      <div className="md:block hidden">
+        {bannerIndex != null && stories[bannerIndex] ? (
+          stories.length &&
           stories.map((item, index) => {
             return index === bannerIndex ? (
               <React.Fragment key={index}>
@@ -175,21 +247,26 @@ const Hero = () => {
                           alt="Picture of the author"
                           sizes="(max-width: 768px) 30vw, (max-width: 1200px) 30vw"
                         />
-                        <div className="absolute flex items-center justify-center z-10 text-white space-y-4 px-10 w-full inset-0 bg-black bg-opacity-50 ">
+                        <button
+                          href="#"
+                          // onClick={() => navigate(`/story/${story?.id}`)}
+                          className="absolute flex items-center justify-center z-10 text-white space-y-4 px-10 w-full inset-0 bg-black bg-opacity-50 "
+                        >
                           <span className="text-lg lg:text-2xl font-bold font-body">
-                            Story 1{data.title}
+                            Story title bottom{data.title}
                           </span>
-                        </div>
+                        </button>
                       </div>
                     ))}
                 </div>
               </React.Fragment>
-            ) : (
-              <div className="flex items-center justify-normal text-2xl">
-                <p>No data found</p>
-              </div>
-            );
-          })}
+            ) : null;
+          })
+        ) : (
+          <div className="flex items-center justify-normal text-2xl">
+            <p>No data found</p>
+          </div>
+        )}
       </div>
     </>
   );
