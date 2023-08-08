4ad9c27aa64126f87e2438b9f742464e69f804d1
matang-7span <matang@7span.com>
2023-08-08 14:53:22 +0530

    validation while fetching data

--- app/research/menuscript/page.jsx
+++ app/research/menuscript/page.jsx
@@ -19,12 +19,14 @@ const page = async () => {
 
   return (
     <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto">
-      <div>
-        <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
-        {arabic_manuscripts_data?.title }
-        </h3>
-        <div dangerouslySetInnerHTML={{ __html: arabic_manuscripts_data?.description }} />
-      </div>
+      {arabic_manuscripts_data && 
+        <div>
+          <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
+          {arabic_manuscripts_data?.title }
+          </h3>
+          <div dangerouslySetInnerHTML={{ __html: arabic_manuscripts_data?.description }} />
+        </div>
+      }
     </div>
   );
 };
