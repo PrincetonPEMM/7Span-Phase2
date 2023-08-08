67604ab651bfbd02547c98644676506a513eb84f
matang-7span <matang@7span.com>
2023-08-08 12:54:19 +0530

    render data of arabic stories

--- app/research/arabicStories/page.jsx
+++ app/research/arabicStories/page.jsx
@@ -1,31 +1,25 @@
+import { client } from "@/utils/directUs";
+import { readItems } from "@directus/sdk";
 import React from "react";
+import { render } from "react-dom";
+import convert from "html-to-jsx";
 
-const page = () => {
+const page = async () => {
+
+  const arabic_stories_data = await client.request(
+    readItems("arabic_stories", {
+      fields: ["*.*.*"],
+      // fields: ["title", "date_created", { authors: ["name"] }],
+    })
+  );
+
   return (
     <div className="container font-body space-y-4 py-12">
       <div>
         <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
-          Repositories and Archives
+          {arabic_stories_data?.title ?? ""}
         </h3>
-        <p className="text-offBlack lg:text-base text-sm">
-          Below we list all the institutions with Tä'ammerä Maryam (Miracle of
-          Mary) manuscripts that PEMM has cataloged. Many of these institutions
-          hold physical copies of G e'e z manuscripts, but some hold only
-          digital copies. To read our cataloging of a particular institution's
-          manuscripts, please use the dropdown on the Manuscripts search pag
-        </p>
-      </div>
-      <div>
-        <h5 className="text-lg lg:text-xl text-primary-500 font-bold mb-2">
-          Repositories and Archives
-        </h5>
-        <p className="text-offBlack text-sm">
-          Below we list all the institutions with Tä'ammerä Maryam (Miracle of
-          Mary) manuscripts that PEMM has cataloged. Many of these institutions
-          hold physical copies of G e'e z manuscripts, but some hold only
-          digital copies. To read our cataloging of a particular institution's
-          manuscripts, please use the dropdown on the Manuscripts search pag
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: arabic_stories_data?.description }} />
       </div>
     </div>
   );
