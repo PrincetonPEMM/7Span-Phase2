47b8b5bdf5efaefcb00c97b5753dedf715be9e44
matang-7span <matang@7span.com>
2023-08-08 14:49:01 +0530

    correct naming convention

--- app/research/research-posts/page.jsx
+++ app/research/research-posts/page.jsx
@@ -3,9 +3,9 @@ import { readItems } from "@directus/sdk";
 import React from "react";
 
 const page = async () => {
-  let arabic_stories_data = null
+  let research_posts_data = null
   try {
-    arabic_stories_data = await client.request(
+    research_posts_data = await client.request(
       readItems("research_posts", {
         fields: ["*.*.*"]
       })
@@ -17,12 +17,12 @@ const page = async () => {
   
   return (
     <div className="container font-body space-y-4 py-12">
-      {arabic_stories_data &&
+      {research_posts_data &&
       <div>
         <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
-          {arabic_stories_data?.title ?? ""}
+          {research_posts_data?.title ?? ""}
         </h3>
-        <div dangerouslySetInnerHTML={{ __html: arabic_stories_data?.description }} />
+        <div dangerouslySetInnerHTML={{ __html: research_posts_data?.description }} />
       </div>
       }
     </div>
