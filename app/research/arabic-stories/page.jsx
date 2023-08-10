283398c18105401b4f44244d933841999ddb05c2
matang-7span <matang@7span.com>
2023-08-10 13:12:03 +0530

    intro section

--- app/research/arabic-stories/page.jsx
+++ app/research/arabic-stories/page.jsx
@@ -6,10 +6,9 @@ const page = async () => {
   let arabic_stories_data = null
   try {
     arabic_stories_data = await client.request(
-      readItems("arabic_stories", {
-        fields: ["*.*.*"]
-      })
+      readItems("arabic_stories")
     );
+    console.log(arabic_stories_data);
   }
   catch (e) {
     console.log(e);
@@ -22,6 +21,7 @@ const page = async () => {
           <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
             {arabic_stories_data?.title ?? ""}
           </h3>
+          <p class="py-6">{ arabic_stories_data?.intro }</p>
           <div dangerouslySetInnerHTML={{ __html: arabic_stories_data?.description }} />
         </div>
       }
