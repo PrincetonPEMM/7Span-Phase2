1981308d9b9788a834704332721065c97afa0dd8
matang-7span <matang@7span.com>
2023-08-11 14:32:12 +0530

    Slider

--- app/components/StoryInfo.jsx
+++ app/components/StoryInfo.jsx
@@ -4,30 +4,48 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Image from "next/image";
 import { Tab } from "@headlessui/react";
 import logo from "../../assets/images/image.png";
+import logo2 from "../../assets/images/logo-white.png";
+import logo3 from "../../assets/images/menuscript-bg.png";
 import SliderModal from "./SliderModal";
 import Tabs from "./tabs";
 const StoryInfo = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [openImage, setOpenImage] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleModal = () => {
-    setIsModalOpen(!setIsOpen);
-  };
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-    fetchPosts({
-      page: 1,
-    });
-  };
+  
   const sliderImg = [
     {
       url: logo,
     },
     {
-      url: logo,
+      url: logo2,
+    },
+    {
+      url: logo3,
     },
     {
       url: logo,
+    },
+    {
+      url: logo2,
+    },
+    {
+      url: logo3,
+    },
+    {
+      url: logo,
+    },
+    {
+      url: logo2,
+    },
+    {
+      url: logo3,
+    },
+    {
+      url: logo,
+    },
+    {
+      url: logo2,
+    },
+    {
+      url: logo3,
     },
   ];
   const data = [
@@ -163,44 +181,11 @@ const StoryInfo = () => {
           <div>
             {/* slider */}
             <div className="grid grid-cols-2 gap-1 py-4 md:grid-cols-4 md:gap-3">
-              {sliderImg?.length &&
-                sliderImg.map((item, index) => {
-                  if (index <= 3) {
-                    return (
-                      <button
-                        key={index}
-                        onClick={() => {
-                          setIsOpen(true);
-                          setOpenImage(item.url);
-                        }}
-                        className="relative"
-                      >
-                        <Image
-                          src={item.url}
-                          className="rounded-md"
-                          alt="PEMM"
-                        />
-                        {index === 3 && (
-                          <div className="absolute inset-0 flex h-full w-full items-center justify-center rounded-md bg-gray-700/50">
-                            <p className="flex items-center gap-1 text-xl text-white md:text-2xl ">
-                              <span>{sliderImg.length - 3}</span>
-                            </p>
-                          </div>
-                        )}
-                      </button>
-                    );
-                  }
-                })}
+              
             </div>
 
             <SliderModal
               sliderImg={sliderImg}
-              isOpen={isOpen}
-              openImage={openImage}
-              modalClose={() => {
-                setIsOpen(false);
-                setOpenImage("");
-              }}
             />
 
             {/* slider content */}
