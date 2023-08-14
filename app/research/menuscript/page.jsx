877a126e9d652d9e60255734aa8a3a805c307219
matang-7span <matang@7span.com>
2023-08-14 12:51:01 +0530

    match design

--- app/research/menuscript/page.jsx
+++ app/research/menuscript/page.jsx
@@ -18,16 +18,18 @@ const page = async () => {
   }
 
   return (
-    <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto bg-background-500">
-      {arabic_manuscripts_data && 
-        <div>
-          <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
-          {arabic_manuscripts_data?.title }
-          </h3>
-          <p class="py-6">{ arabic_manuscripts_data?.intro }</p>
-          <div dangerouslySetInnerHTML={{ __html: arabic_manuscripts_data?.description }} />
-        </div>
-      }
+    <div className="bg-background-500">
+      <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto">
+        {arabic_manuscripts_data && 
+          <div>
+            <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
+            {arabic_manuscripts_data?.title }
+            </h3>
+            <p class="py-6">{ arabic_manuscripts_data?.intro }</p>
+            <div dangerouslySetInnerHTML={{ __html: arabic_manuscripts_data?.description }} />
+          </div>
+        }
+      </div>
     </div>
   );
 };
