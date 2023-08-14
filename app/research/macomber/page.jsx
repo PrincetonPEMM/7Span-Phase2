5eecb7b77d74e0d25981dac327906988112991b8
matang-7span <matang@7span.com>
2023-08-14 12:53:30 +0530

    match design

--- app/research/macomber/page.jsx
+++ app/research/macomber/page.jsx
@@ -18,16 +18,18 @@ const page = async () => {
   }
 
   return (
-    <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto bg-background-500">
-      {macomber_handlist_data && 
-        <div>
-          <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
-          {macomber_handlist_data?.title }
-          </h3>
-          <p class="py-6">{ macomber_handlist_data?.intro }</p>
-          <div dangerouslySetInnerHTML={{ __html: macomber_handlist_data?.description }} />
-        </div>
-      }
+    <div className="bg-background-500">
+      <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto">
+        {macomber_handlist_data && 
+          <div>
+            <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
+            {macomber_handlist_data?.title }
+            </h3>
+            <p class="py-6">{ macomber_handlist_data?.intro }</p>
+            <div dangerouslySetInnerHTML={{ __html: macomber_handlist_data?.description }} />
+          </div>
+        }
+      </div>
     </div>
   );
 };
