f961cd27c84e9e3910e9e2ea119ba90a3d612e9a
keya-7span <keya@7span.com>
2023-08-14 18:11:12 +0530

    menuscript index page

--- app/menuscript/page.jsx
+++ app/menuscript/page.jsx
@@ -1,18 +1,10 @@
 import React from "react";
+import ManuScript from "../components/ManuScripts";
 
 const page = () => {
   return (
-    <div className="container">
-      <h3 className="lg:text-5xl text-primary-500 font-bold text-xl">
-        Repositories and Archives
-      </h3>
-      <p className="text-sm lg:text-base">
-        Below we list all the institution with (Miracle of marry) manuscripts
-        that PEMM has caraloged.Many of these institution hold physical copies
-        of Ge'ex manuscript, but some hold only digital copies. To read our
-        cataloging of a perticular institution's manuscripts, please use the
-        dropdwon on the Manuscripts search page.
-      </p>
+    <div>
+      <ManuScript />
     </div>
   );
 };
