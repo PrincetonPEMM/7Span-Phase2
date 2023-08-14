765167fcf7975e8e7d2c7e439b5151c322c78feb
matang-7span <matang@7span.com>
2023-08-14 12:04:11 +0530

    match background color

--- app/research/macomber/page.jsx
+++ app/research/macomber/page.jsx
@@ -18,7 +18,7 @@ const page = async () => {
   }
 
   return (
-    <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto">
+    <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto bg-background-500">
       {macomber_handlist_data && 
         <div>
           <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
