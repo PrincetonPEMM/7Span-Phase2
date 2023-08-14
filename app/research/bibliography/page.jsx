d61ee876d8e9853880d54e67e0720ea1147f832d
matang-7span <matang@7span.com>
2023-08-14 12:54:48 +0530

    match design

--- app/research/bibliography/page.jsx
+++ app/research/bibliography/page.jsx
@@ -16,16 +16,18 @@ const page = async () => {
   }
   
   return (
-    <div className="container font-body space-y-4 py-12 bg-background-500">
-      {bibliography_data &&
-      <div>
-        <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
-          {bibliography_data?.title ?? ""}
-        </h3>
-        <p class="py-6">{ bibliography_data?.intro }</p>
-        <div dangerouslySetInnerHTML={{ __html: bibliography_data?.description }} />
+    <div className="bg-background-500">
+      <div className="container font-body space-y-4 py-12">
+        {bibliography_data &&
+        <div>
+          <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
+            {bibliography_data?.title ?? ""}
+          </h3>
+          <p class="py-6">{ bibliography_data?.intro }</p>
+          <div dangerouslySetInnerHTML={{ __html: bibliography_data?.description }} />
+        </div>
+        }
       </div>
-      }
     </div>
   );
 };
