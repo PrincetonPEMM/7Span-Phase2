0523303306c1551fb4128724d4c083d4f729dd7e
matang-7span <matang@7span.com>
2023-08-17 10:34:58 +0530

    window issue

--- app/components/Masonry.jsx
+++ app/components/Masonry.jsx
@@ -5,13 +5,13 @@ import { useEffect, useState } from "react";
 export default function Masonry(props) {
 
     // noOfColumns state with default value of 3
-    const [noOfColumns, setNoOfColumns] = useState(typeof window == "undefien"? (window.innerWidth >= 1024)?3:(window.innerWidth >= 768?2:1):3);
+    const [noOfColumns, setNoOfColumns] = useState(3);
     // change no of coloum on window resize
     useEffect(() => {
         window.addEventListener("resize", function () {
             setNoOfColumns((window.innerWidth >= 1024)?3:(window.innerWidth >= 768?2:1))
         });
-        
+        setNoOfColumns((window.innerWidth >= 1024)?3:(window.innerWidth >= 768?2:1))
     }, []);
 
     return (
