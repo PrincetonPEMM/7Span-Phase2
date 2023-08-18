25cbd3781c57c3a3854bc6abff0f64a6426df18f
matang-7span <matang@7span.com>
2023-08-18 15:11:31 +0530

    Directus HTML styling

--- app/research/repositories/page.jsx
+++ app/research/repositories/page.jsx
@@ -16,17 +16,19 @@ const page = async () => {
   }
 
   return (
-      <div className="container-fluid font-body space-y-4 py-12 bg-background-500">
+    <div className="container-fluid">
+      <div className="font-body space-y-4 py-12 bg-background-500">
         {repositories_data && 
           <div>
             <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
               {repositories_data?.title ?? ""}
             </h3>
             <p class="py-6">{ repositories_data?.intro }</p>
-            <div dangerouslySetInnerHTML={{ __html: repositories_data?.description }} />
+            <div dangerouslySetInnerHTML={{ __html: repositories_data?.description }} className="space-y-p" />
           </div>
         }
       </div>
+    </div>
   );
 };
 
