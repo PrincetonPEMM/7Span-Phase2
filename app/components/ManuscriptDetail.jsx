5db06b33615b6181a98b62766a2feef5061cc117
keya-7span <keya@7span.com>
2023-08-18 15:21:32 +0530

    menuscript detail page WIP

--- app/components/ManuscriptDetail.jsx
+++ app/components/ManuscriptDetail.jsx
@@ -1,12 +1,49 @@
 import React from "react";
+import Table from "./Table";
 
 const ManuscriptDetail = ({ menudetail }) => {
+  const tableData = [
+    { earliest_attestation: "hello" },
+    {
+      canonical_story_id: "hello",
+    },
+  ];
+  const tableHeader = [
+    {
+      name: "id",
+    },
+    {
+      name: "Story TItle",
+    },
+    {
+      name: "Id Certa inty",
+    },
+    {
+      name: "Location in MS",
+    },
+    {
+      name: "Location in MS",
+    },
+    {
+      name: "Inipit",
+    },
+    {
+      name: "Painitngs",
+    },
+  ];
   return (
-    <div>
-      <h2>British L ibrary "Orient" Collection, M anuscript N o . 520</h2>
-      {menudetail.map((data, index) => {
-        <p key={index}>{data.text}</p>;
-      })}
+    <div className="container-fliud">
+      <h2 className="font-menu text-5xl font-medium">
+        British Library "Orient" Collection, Manuscript No . 520
+      </h2>
+      <div className="space-y-p">
+        {menudetail.map((data, index) => (
+          <p key={index}>{data.text}</p>
+        ))}
+      </div>
+      <h3 className="font-menu text-5xl font-medium">EMIP (EMIP) 981a</h3>
+
+      <Table tableData={tableData} tableHeader={tableHeader} />
     </div>
   );
 };
--- app/manuscript-detail/page.jsx
+++ app/manuscript-detail/page.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import ManuscriptDetail from "../components/ManuscriptDetail";
 
 const page = () => {
-  const menudetail = [
+  const menudetails = [
     {
-      text: "lorem ipsum dolor site amrt ",
+      text: "This Gəˁəz (classical Ethiopic) language manuscript was created between 1907 and 1908. This date is precise, based on the scribe noting the date in the manuscript.",
+    },
+    {
+      text: "This manuscript's last known location (i.e., where it was microfilmed or digitized at some point in the past forty years) is the repository of Mihur Eyesus Monastery in Gurage Province, Ethiopia. A digital copy of this manuscript is held by Ethiopian Manuscript Imaging Project, Hill Museum and Manuscript Library & Beta Masaheft in Portland, OR , United States.",
+    },
+    {
+      text: "This manuscript's last known location (i.e., where it was microfilmed or digitized at some point in the past forty years) is the repository of Mihur Eyesus Monastery in Gurage Province, Ethiopia. A digital copy of this manuscript is held by Ethiopian Manuscript Imaging Project, Hill Museum and Manuscript Library & Beta Masaheft in Portland, OR , United States.",
+    },
+    {
+      text: "This manuscript's last known location (i.e., where it was microfilmed or digitized at some point in the past forty years) is the repository of Mihur Eyesus Monastery in Gurage Province, Ethiopia. A digital copy of this manuscript is held by Ethiopian Manuscript Imaging Project, Hill Museum and Manuscript Library & Beta Masaheft in Portland, OR , United States.",
+    },
+    {
+      text: "This manuscript's last known location (i.e., where it was microfilmed or digitized at some point in the past forty years) is the repository of Mihur Eyesus Monastery in Gurage Province, Ethiopia. A digital copy of this manuscript is held by Ethiopian Manuscript Imaging Project, Hill Museum and Manuscript Library & Beta Masaheft in Portland, OR , United States.",
     },
   ];
   return (
     <div>
-      <ManuscriptDetail menudetail={menudetail} />
+      <ManuscriptDetail menudetail={menudetails} />
     </div>
   );
 };
