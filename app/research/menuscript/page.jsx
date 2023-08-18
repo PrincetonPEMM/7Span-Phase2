4537a1c7edc68b35ac76aadb04e4765e769c9766
matang-7span <matang@7span.com>
2023-08-18 12:12:11 +0530

    remove container for full width

--- app/research/menuscript/page.jsx
+++ app/research/menuscript/page.jsx
@@ -16,8 +16,7 @@ const page = async () => {
   }
 
   return (
-    <div className="bg-background-500">
-      <div className="container font-body space-y-4 py-12 max-w-screen-2xl mr-auto">
+      <div className="font-body space-y-4 py-12 max-w-screen-2xl mr-auto bg-background-500 px-5">
         {arabic_manuscripts_data && 
           <div>
             <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
@@ -28,7 +27,6 @@ const page = async () => {
           </div>
         }
       </div>
-    </div>
   );
 };
 
