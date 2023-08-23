ed68c3b6e44db8c06ec76ccacd5862b4c92e6ee9
keya-7span <keya@7span.com>
2023-08-23 14:21:47 +0530

    reserach page review and change css

--- app/research/manuscript/page.jsx
+++ app/research/manuscript/page.jsx
@@ -20,7 +20,7 @@ const page = async () => {
       <div className="font-body space-y-4 py-12 max-w-screen-2xl mr-auto bg-background-500">
         {arabic_manuscripts_data && (
           <div>
-            <h3 className="text-3xl lg:text-5xl text-primary-500 font-bold ">
+            <h3 className="text-3xl text-primary-500 font-bold  lg:text-5xl">
               {arabic_manuscripts_data?.title}
             </h3>
             <p class="py-6">{arabic_manuscripts_data?.intro}</p>
