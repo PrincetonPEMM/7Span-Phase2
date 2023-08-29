69a359e4ac369e4a3bf4f8e3f971d2880d10ec24
keya-7span <keya@7span.com>
2023-08-29 18:38:29 +0530

    Painting detail page

---
+++ app/painting-detail/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PaintingDetail from "../components/PaintingDetail";
+
+const page = () => {
+  return (
+    <div>
+      <PaintingDetail />
+    </div>
+  );
+};
+
+export default page;
