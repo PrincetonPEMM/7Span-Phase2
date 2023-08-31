1ab8e032b9447b2ed89620447cf8f45ea6755088
Maulik Savaliya <maulik@7span.com>
2023-08-31 19:33:03 +0530

    Pagination and dynamic filter integrated.

--- app/components/PaintingCard.jsx
+++ app/components/PaintingCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import { defaultImageforPainting } from "@/utils/constant";
 
 const PaintingCard = (props) => {
   return (
@@ -8,20 +8,39 @@ const PaintingCard = (props) => {
     >
       <div className=" bg-offWhite-500">
         <img
-          src={props.card.image_link}
-          alt="PEMM"
+          src={
+            props.card.image_link
+              ? props.card.image_link
+              : defaultImageforPainting
+          }
+          alt={props.card.pemm_short_title}
           className="w-full object-cover "
         />
       </div>
       <div className="bg-black p-5">
-        <p className="text-xs">{`${props.card.manuscript_date_range_start}-${props.card.manuscript_date_range_start},${props.card.manuscript} f.${props.card.painting_folio} s.${props.card.painting_scan}`}</p>
+        <p className="text-xs">{`${
+          props.card.manuscript_date_range_start &&
+          props.card.manuscript_date_range_start
+            ? props.card.manuscript_date_range_start +
+              "-" +
+              props.card.manuscript_date_range_start
+            : "-"
+        }${props.card.manuscript ? "," + props.card.manuscript : "-"} ${
+          props.card.painting_folio ? "f." + props.card.painting_folio : ""
+        } ${
+          props.card.painting_scan ? "s." + props.card.painting_scan : ""
+        }`}</p>
         <h2 className="lg:text-2xl font-bold mt-3">
-          {props.card.pemm_short_title}
+          {props.card.pemm_short_title
+            ? props.card.pemm_short_title
+            : "Title not Found"}
         </h2>
-        <p className="pt-2">{props.card.episodes}</p>
+        <p className="pt-2">
+          {props.card.episodes ? props.card.episodes : "-"}
+        </p>
       </div>
     </div>
   );
 };
 
-export default PaintingCard;
+export default React.memo(PaintingCard);
