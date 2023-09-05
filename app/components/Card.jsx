72fd3221dd833c13cf694e3edab822b10d6a1878
keya-7span <keya@7span.com>
2023-09-05 13:48:52 +0530

    manuscript detail page

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -13,16 +13,18 @@ export default function Card({
     <div
       className={`rounded-lg text-white px-5 md:px-8 py-10 relative inline-block my-3 card-background`}
     >
-      <buttion
-        className={`absolute top-3 right-3 text-xs px-2 py-1 card-button rounded-md`}
+      <button
+        className={`absolute top-3 inline-block right-3 text-xs px-2 py-1 card-button rounded-md leading-tight`}
       >
         {link_text}
-      </buttion>
+      </button>
       <h2 className="lg:text-2xl font-bold">{title}</h2>
-      <span className="my-2">{date}</span>
+      <span className="my-2 font-light">{date}</span>
       <h3 className="font-semibold">{author}</h3>
-      <p className="py-2">{description}</p>
-      <buttion className="border p-1 rounded">Read more</buttion>
+      <p className="py-2 text-sm">{description}</p>
+      <button className="border py-1 px-2 rounded inline-block transition-colors hover:text-primary-500 hover:bg-offWhite-500 hover:transition-colors">
+        Read More
+      </button>
     </div>
   );
 }
