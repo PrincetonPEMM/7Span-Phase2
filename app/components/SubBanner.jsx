dd3a74df0eb6a6b608b9cd5c89bc938ed42c16b4
keya-7span <keya@7span.com>
2023-09-11 12:38:30 +0530

    set Image content on load to display

--- app/components/SubBanner.jsx
+++ app/components/SubBanner.jsx
@@ -1,9 +1,15 @@
+import React, { useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React from "react";
 
 const SubBanner = ({ stories, divClass }) => {
   const route = useRouter();
 
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
   return (
     <div className="grid sm:grid-cols-3">
       {stories &&
@@ -14,19 +20,23 @@ const SubBanner = ({ stories, divClass }) => {
             onClick={() => route.push(data?.redirectLink)}
           >
             <div className="Subbanner_img">
-              <img
+              <Image
                 src={data?.img}
                 alt="Picture of the author"
+                width={500}
+                height={500}
                 sizes="(max-width: 768px) 30vw, (max-width: 1200px) 30vw"
                 style={{ width: "100%", height: "100%" }}
+                onLoad={handleImageLoad}
               />
             </div>
-
-            <button className="absolute flex items-center justify-center z-10 text-white space-y-4 px-10 w-full inset-0">
-              <span className="text-lg lg:text-2xl font-bold font-body">
-                {data?.title}
-              </span>
-            </button>
+            {imageLoaded && (
+              <button className="absolute flex items-center justify-center z-10 text-white space-y-4 px-10 w-full inset-0">
+                <span className="text-lg lg:text-2xl font-bold font-body">
+                  {data?.title}
+                </span>
+              </button>
+            )}
           </div>
         ))}
     </div>
