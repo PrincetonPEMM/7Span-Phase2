19b786b127d62fd0a9bdafcc8bf19f65ecb5aacf
maulik-7span <140496633+maulik-7span@users.noreply.github.com>
2023-09-21 05:12:50 +0000

    fixes contact us and using the site page.

---
+++ app/about/connect/contact-us/page.jsx
@@ -0,0 +1,39 @@
+import { client } from "@/utils/directUs";
+import { readItems } from "@directus/sdk";
+export const dynamic = "force-dynamic";
+
+export default async function page() {
+  let about_using_this_site = null;
+
+  try {
+    about_using_this_site = await client.request(
+      readItems("about_using_this_site", {
+        fields: ["*.*.*"],
+      })
+    );
+  } catch (e) {
+    console.log(e);
+    console.log(about_using_this_site);
+  }
+
+  return (
+    <div className="container">
+    <div className="space-y-4 py-12">
+      <div className="people flex justify-center flex-col items-center">
+        <h2 className="text-3xl font-header text-center lg:text-5xl">
+          {about_using_this_site.contact_us_title}
+        </h2>
+        <p className="text-center font-menu py-5 lg:w-2/3">
+          {about_using_this_site.contact_us_intro}
+        </p>
+        <div
+          className="space-y-p text-center font-menu lg:w-2/3 md:text-center"
+          dangerouslySetInnerHTML={{
+            __html: about_using_this_site.contact_us_description,
+          }}
+        />
+      </div>
+    </div>
+  </div>
+  );
+}
