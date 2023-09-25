c41085093718cce412edb943248757c2a46e7117
Maulik Savaliya <maulik@7span.com>
2023-09-25 16:05:32 +0530

    Make new Pagination components.

--- app/components/ManuScripts.jsx
+++ app/components/ManuScripts.jsx
@@ -23,7 +23,7 @@ import {
   rangeSliderMaxUniqueStoriesManuscriptsPage,
 } from "@/utils/constant";
 import useDebounce from "@/utils/useDebounce";
-import { TablePagination } from "./Pagination";
+import CustomPagination, { TablePagination } from "./Pagination";
 
 const ManuScripts = () => {
   const [expandedRows, setExpandedRows] = useState([]);
@@ -295,9 +295,18 @@ const ManuScripts = () => {
               }}
             />
           </div>{" "}
-          <p className="text-offBlack-400 font-medium pl-2 text-sm lg:text-base col-span-2 sm:text-center">
+          <p className="text-offBlack-400 font-medium pl-2 text-sm lg:text-base col-span-2 xl:col-span-1 sm:text-center">
             Results: {`(${totalPage ? totalPage : 0} records)`}
           </p>
+          <CustomPagination
+            className="pagination-tablet"
+            currentPage={page}
+            totalPages={Math.ceil(totalPage / perPage)}
+            onPageChange={(num) => {
+              setPage(num);
+              setExpandedRows([]);
+            }}
+          />
           <div className="w-full mt-2 sm:mt-0 flex items-center justify-end gap-3 text-sm 2xl:text-base">
             <button
               className="bg-primary-500 text-white max-w-fit w-auto px-2 py-3 md:py-3 md:px-4 font-semibold text-xs md:text-sm rounded-md lg:hover:text-primary-500 uppercase lg:hover:bg-transparent lg:hover:border-primary-500 border-2 border-primary-500 transition-colors lg:hover:transition-colors"
@@ -343,7 +352,7 @@ const ManuScripts = () => {
             {isLoading ? <h1>Loading...</h1> : <h1>Records Not Found</h1>}
           </div>
         )}
-        <TablePagination
+        {/* <TablePagination
           meta={{
             total: totalPage,
             per_page: perPage,
@@ -360,7 +369,7 @@ const ManuScripts = () => {
             });
             setExpandedRows([]);
           }}
-        />
+        /> */}
         {/* </div> */}
       </div>
     </div>
--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -1,4 +1,5 @@
 import MdiArrowUp from "@/assets/icons/MdiArrowUp";
+import { useState } from "react";
 import Pagination from "react-js-pagination";
 
 export const TablePagination = ({ meta, isOpen, onPageChange, ...rest }) => {
@@ -20,47 +21,125 @@ export const TablePagination = ({ meta, isOpen, onPageChange, ...rest }) => {
   );
 };
 
-export const PaginationLoader = () => {
-  return (
-    <>
-      <div className="relative h-10">
-        <div className="mb-4 flex h-1.5 overflow-hidden text-xs">
-          <div
-            style={{ width: "100%" }}
-            className="h-full w-0 animate-ping bg-blue-500"
-          ></div>
-        </div>
-      </div>
-    </>
-  );
-};
+// export const PaginationLoader = () => {
+//   return (
+//     <>
+//       <div className="relative h-10">
+//         <div className="mb-4 flex h-1.5 overflow-hidden text-xs">
+//           <div
+//             style={{ width: "100%" }}
+//             className="h-full w-0 animate-ping bg-blue-500"
+//           ></div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-const NextPage = ({ meta }) => {
-  return (
-    <>
-      <span
-        className={`flex h-10 min-w-[50px] items-center justify-center p-2 text-base ${
-          meta.current_page === meta.last_page
-            ? " text-slate-300"
-            : "text-primary-400"
-        }`}
-      >
-        <MdiArrowUp className="h-6 w-6 rotate-90" />
-      </span>
-    </>
-  );
-};
+// const NextPage = ({ meta }) => {
+//   return (
+//     <>
+//       <span
+//         className={`flex h-10 min-w-[50px] items-center justify-center p-2 text-base ${
+//           meta.current_page === meta.last_page
+//             ? " text-slate-300"
+//             : "text-primary-400"
+//         }`}
+//       >
+//         <MdiArrowUp className="h-6 w-6 rotate-90" />
+//       </span>
+//     </>
+//   );
+// };
 
-const PrevPage = ({ meta }) => {
+// const PrevPage = ({ meta }) => {
+//   return (
+//     <>
+//       <span
+//         className={`flex h-10 min-w-[50px] items-center justify-center p-2 text-base ${
+//           meta.current_page === 1 ? " text-slate-300" : "text-primary-400"
+//         }`}
+//       >
+//         <MdiArrowUp className="h-6 w-6 -rotate-90" />
+//       </span>
+//     </>
+//   );
+// };
+
+function CustomPagination({
+  className,
+  currentPage,
+  totalPages,
+  onPageChange,
+}) {
+  const [inputValue, setInputValue] = useState(currentPage);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setInputValue(newPage);
+      onPageChange(newPage);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    if (+value < 1) {
+      setInputValue(1);
+    } else setInputValue(value);
+  };
+
+  const handleInputKeyPress = (event) => {
+    if (event.key === "Enter") {
+      const value = parseInt(inputValue);
+      if (!isNaN(value)) {
+        handlePageChange(value);
+      }
+    }
+  };
+
   return (
-    <>
+    <div className={`pagination-container ${className}`}>
       <span
-        className={`flex h-10 min-w-[50px] items-center justify-center p-2 text-base ${
-          meta.current_page === 1 ? " text-slate-300" : "text-primary-400"
-        }`}
+        className={`pagination-button ${currentPage === 1 ? "disabled" : ""}`}
+        onClick={() => handlePageChange(1)}
       >
-        <MdiArrowUp className="h-6 w-6 -rotate-90" />
+        «
       </span>
-    </>
+      <span
+        className={`pagination-button ${currentPage === 1 ? "disabled" : ""}`}
+        onClick={() => handlePageChange(currentPage - 1)}
+      >
+        ⟨
+      </span>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyPress={handleInputKeyPress}
+        min={1}
+        // placeholder={`Page ${currentPage}`}
+      />
+      <span className="page-total text-primary-600">
+        of {totalPages ? totalPages : 0}
+      </span>
+      <span
+        className={`pagination-button ${
+          currentPage === totalPages ? "disabled" : ""
+        }`}
+        onClick={() => handlePageChange(currentPage + 1)}
+      >
+        ⟩
+      </span>
+      <span
+        className={`pagination-button ${
+          currentPage === totalPages ? "disabled" : ""
+        }`}
+        onClick={() => handlePageChange(totalPages)}
+      >
+        »
+      </span>
+    </div>
   );
-};
+}
+
+export default CustomPagination;
