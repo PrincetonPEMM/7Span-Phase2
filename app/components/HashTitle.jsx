b9c465272de3776c16fdd704f794b8a4c0625d28
Maulik Savaliya <maulik@7span.com>
2023-10-30 14:27:59 +0530

    fixes hashroute issue.

---
+++ app/components/HashTitle.jsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useEffect } from "react";
+
+const HashTitle = ({ title, id }) => {
+  useEffect(() => {
+    const targetDiv = document.getElementById(location?.hash.slice(1));
+    if (targetDiv) {
+      targetDiv.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }, [id, location?.hash]);
+
+  return (
+    <h2
+      className="text-3xl font-header text-center lg:text-5xl text-primary-500"
+      id={id}
+    >
+      {title}
+    </h2>
+  );
+};
+
+export default HashTitle;
