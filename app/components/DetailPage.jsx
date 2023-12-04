51f943bdcaefe60cfb796c74f9e7f5cc807787d0
keya-7span <keya@7span.com>
2023-12-04 20:15:26 +0530

    design changes

--- app/components/DetailPage.jsx
+++ app/components/DetailPage.jsx
@@ -11,28 +11,28 @@ const DetailPage = ({ image, title, author, date, intro, description }) => {
           <h2 className="text-3xl text-primary-500 font-bold lg:text-4xl font-body">
             {title ?? ""}
           </h2>
-          <div className="items-start font-body pb-7 pt-5 lg:grid lg:grid-cols-3 lg:space-x-10">
-            {image && (
-              <div className="w-60 aspect-squre md:mx-auto sm:h-80 sm:w-80 md:w-full">
-                <img
-                  src={`${image}`}
-                  alt={title}
-                  className="object-cover object-center w-60 sm:h-60 lg:h-80 xl:w-full"
-                />
-              </div>
-            )}
+          <div className="font-body pb-7 pt-5">
             <div class="mt-5 lg:mt-0 lg:col-span-2">
+              <img
+                src={`${image}`}
+                alt={title}
+                className="w-60 object-cover object-center float-left mr-5 sm:h-60 lg:h-80"
+              />
+
               <p className="pb-2 font-sm font-bold">By {author}</p>
               <p className="py-2 font-light text-sm">{dateFormate(date)}</p>
               <p className="py-2">{intro}</p>
+
+              {/* <p className="py-2">{description}</p> */}
+
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: description,
+                }}
+                className="space-y-p contents"
+              />
             </div>
           </div>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: description,
-            }}
-            className="space-y-p descriptions-left"
-          />
         </div>
       </div>
     </div>
