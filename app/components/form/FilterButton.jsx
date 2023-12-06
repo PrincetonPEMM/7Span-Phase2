f11d03b1870102c5a2d96f8f6d94268ce8f193b0
Maulik Savaliya <maulik@7span.com>
2023-12-06 18:27:00 +0530

    fixes web accessiblity issue.

--- app/components/form/FilterButton.jsx
+++ app/components/form/FilterButton.jsx
@@ -1,9 +1,14 @@
 import MdiMenuOpen from "@/assets/icons/MdiMenuOpen";
 import React from "react";
 
-const FilterButton = ({ onClick, areaLabel, className }) => {
+const FilterButton = ({ onClick, areaLabel, className, _id = "" }) => {
   return (
-    <button onClick={onClick} area-label={areaLabel} className={className}>
+    <button
+      id={_id}
+      onClick={onClick}
+      area-label={areaLabel}
+      className={className}
+    >
       <MdiMenuOpen className="text-white-500 h-6 w-6 text-inherit" />
     </button>
   );
