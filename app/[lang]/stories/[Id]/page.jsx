e0d93854ed1cc825b0be9e2822ba8b6cb5c9e137
Maulik Savaliya <maulik@7span.com>
2024-02-15 12:26:59 +0530

    Converted Story detail page.

--- app/[lang]/stories/[Id]/page.jsx
+++ app/[lang]/stories/[Id]/page.jsx
@@ -1,3 +1,5 @@
+import { client } from "@/utils/directUs";
+import { readItems } from "@directus/sdk";
 import Script from "next/script";
 import StoryDetail from "../../components/StoryDetail";
 
@@ -5,6 +7,18 @@ export const dynamic = "force-dynamic";
 const Page = async ({ params }) => {
   const { Id } = params;
   let data = null;
+
+  let languages = await client.request(
+    readItems("languages", { fields: ["*.*.*"] })
+  );
+
+  const selectedLanguage = languages.filter((tt) => tt.code === params.lang)[0];
+
+  let localData = await fetch(
+    `${process.env.NEXT_PUBLIC_DIRECTUS_URL}string_localization?language=${params.lang}`
+  );
+
+  localData = await localData.json();
 
   try {
     const response = await fetch(
@@ -28,7 +42,7 @@ const Page = async ({ params }) => {
           gtag('config', 'G-L1XB3HXBQM');
         `}
       </Script>
-      <StoryDetail data={data} Id={Id} />
+      <StoryDetail data={data} Id={Id} localData={localData} />
     </main>
   );
 };
