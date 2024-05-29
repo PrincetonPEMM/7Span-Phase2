9d6ac0863f5f7050e941b3428fdc70cb6e28b247
Maulik Savaliya <maulik@7span.com>
2024-05-29 11:20:03 +0530

    Fixes manuscirpt filter bugs.

--- app/[lang]/manuscripts/page.jsx
+++ app/[lang]/manuscripts/page.jsx
@@ -1,10 +1,11 @@
+import { i18n } from "@/i18n";
 import ManuScripts from "../components/ManuScripts";
 
 export const dynamic = "force-dynamic";
 
 const page = async ({ params }) => {
   const localStr = await fetch(
-    `${process.env.NEXT_PUBLIC_DIRECTUS_URL}string_localization?language=${params.lang}`
+    `${process.env.NEXT_PUBLIC_DIRECTUS_URL}string_localization?language=${i18n.defaultLocale}` //params.lang
   );
   const localData = await localStr.json();
 
